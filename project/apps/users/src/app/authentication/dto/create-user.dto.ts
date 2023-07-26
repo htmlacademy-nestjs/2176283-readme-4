6ef3dba0360ea5992e5123da88fcccebb54ad1c4
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-07-26 23:23:19 +0300

    2.29 Воспользуемся `@nestjs/swagger` для формирования спецификации
    В точке входа `main.ts` настраиваем build.

--- project/apps/users/src/app/authentication/dto/create-user.dto.ts
+++ project/apps/users/src/app/authentication/dto/create-user.dto.ts
@@ -1,6 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User unique address',
+    example: 'user@bk.ru'
+  })
   public email: string;
+
+  @ApiProperty({
+    description: 'User name',
+    example: 'Mike Wazowski',
+  })
   public name: string;
+
+  @ApiProperty({
+    description: 'User password',
+    example: '123456',
+  })
   public password: string;
+
+  @ApiProperty({
+    description: 'User avatar',
+    example: '/images/avatar.png'
+  })
   public avatar: string;
 }
--- project/apps/users/src/app/authentication/rdo/logged-user.rdo.ts
+++ project/apps/users/src/app/authentication/rdo/logged-user.rdo.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class LoggedUserRdo {
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '6',
+  })
   @Expose({name: 'id'})
   public id: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@bk.ru',
+  })
   @Expose()
   public email: string;
 
+  @ApiProperty({
+    description: 'Access token',
+    example: 'user@user.local',
+  })
   @Expose()
   public accessToken: string;
 }

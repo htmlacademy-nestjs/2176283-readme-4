68038b5d782a3dcc12a5ee7a2db0b2122c1db138
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-08-09 22:22:00 +0300

    3.10. Добавит ещё один вариант получения настроек Этот коммит только для демонстрации возможностей работы с модулем конфигурации. Мы выполнили все подготовительные действия, посмотрим как можно получить подготовленные настройки. Есть несколько способов это сделать.
    Внедрим сервис конфигурации в качестве зависимости в сервис `AuthenticationService`. Для этого достаточно объявить новый параметр в конструкторе `AuthenticationService` типа `ConfigService`. Остальное сделает Nest.
    
    Чтобы вывести значение, установленное через переменную окружения
    `MONGO_HOST` достаточно обратиться к соответствующему пространству имён.
    Мы обращаемся с помощью метода `get` и считываем значение `host` из
    пространства имён `database`. Для получения значения мы используем
    точечную нотацию: `<namespace>.<property>`.

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,23 +1,21 @@
-import { ConflictException, Inject, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { BlogUserMemoryRepository } from '../blog-user/blog-user-memory.repository';
 import { CreateUserDto } from './dto/create-user.dto';
 import {AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG} from './authentication.constant';
 import { BlogUserEntity } from '../blog-user/blog-user.entity';
 import { LoginUserDto } from './dto/login-user.dto';
-import { dbConfig } from '@project/config/config-users';
-import { ConfigType } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthenticationService {
   constructor(
     private readonly blogUserRepository: BlogUserMemoryRepository,
+    private readonly configService: ConfigService
 
-    @Inject(dbConfig.KEY)
-    private readonly databaseConfig: ConfigType<typeof dbConfig>,
   ) {
     //извлекаем настройки из конфигурации
-    console.log(databaseConfig.host);
-    console.log(databaseConfig.user);
+    console.log(configService.get<string>('db.host'));
+    console.log(configService.get<string>('db.user'));
   }
 
   public async register(dto: CreateUserDto) {

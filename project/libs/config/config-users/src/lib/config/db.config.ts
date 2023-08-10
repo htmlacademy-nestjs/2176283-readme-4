3a672fb372dd46f498def6aae5af75d3a9f55db3
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-08-10 22:09:31 +0300

    3.16. Добавит функцию getMongooseOptions. Теперь обсудим работу с обёрткой для Mongoose. Речь о `@nestjs/mongoose`. Обёртка предоставляет отдельный модуль, упрощающий интеграцию с Mongo. Для этого он предоставляет отдельный объект `MongooseModule`. Общий принцип работы с этим объектом точно такой же как с `ConfigModule`: импортируем из пакета, передаём в импорты (`imports`) главного модуля приложения.
    Дальше мы можем воспользоваться методом `forRoot` и передать настройки для подключения к СУБД. Есть только одно «но». Мы заранее не знаем про настройки. Мы их получаем через `ConfigModule`.
    
    Поэтому, простой вариант нам не подойдёт. Нужно дождаться, когда
    загрузятся другие модули, а лишь потом приступать к загрузке модуля
    для работы с Mongoose.
    
    В связи с этим мы воспользуемся методом `forRootAsync`. Методу нужно
    передать настройки типа `MongooseModuleAsyncOptions`. Для этого мы
    подготовим отдельную функцию `getMongooseOptions`. Он вернёт фабрику, в которую мы внедрим сервис управления конфигурацией (`configService`).
    
    Чтобы внедрение зависимостей сработало, необходимо не забыть указать
    `ConfigService` в качестве внедряемого сервиса в свойстве `inject`.
    Внутри `getMongooseOptions()` воспользуемся ранее подготовленной функцией `getMongoConnectionString`.

--- project/libs/config/config-users/src/lib/config/db.config.ts
+++ project/libs/config/config-users/src/lib/config/db.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 import { plainToInstance } from 'class-transformer';
-import { DatabaseEnvironment } from './database-environment';
+import { DatabaseEnvironment } from '../database-environment';
 import { validateSync } from 'class-validator';
 
 const DEFAULT_MONGO_PORT = 27017;

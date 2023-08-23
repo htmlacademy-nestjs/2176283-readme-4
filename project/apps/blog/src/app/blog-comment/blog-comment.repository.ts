a47bd6900b1306241dd06913043d27bffcbfd81d
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-08-23 22:19:44 +0300

    4.21. Добавит BlogCommentRepository. Подготовим отдельный репозиторий для управления комментариями.

---
+++ project/apps/blog/src/app/blog-comment/blog-comment.repository.ts
@@ -0,0 +1,35 @@
+import { BlogCommentEntity } from './blog-comment.entity';
+import { Comment } from '@project/shared/app-types';
+import { Injectable } from '@nestjs/common';
+import { CRUDRepository } from '@project/util/util-types';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class BlogCommentRepository implements CRUDRepository<BlogCommentEntity, number, Comment> {
+  constructor(private readonly prisma: PrismaService) {}
+
+  public async create(item: BlogCommentEntity): Promise<Comment> {
+      return this.prisma.comment.create({
+        data: { ...item.toObject() }
+      });
+  }
+
+  public async destroy(commentId: number): Promise<void> {
+      await this.prisma.comment.delete({
+        where: { commentId }
+      });
+  }
+
+  public findById(commentId: number): Promise<Comment | null> {
+      return this.prisma.comment.findFirst({
+        where: { commentId }
+      });
+  }
+
+  public update(commentId: number, item: BlogCommentEntity): Promise<Comment> {
+    return this.prisma.comment.update({
+      where: { commentId },
+      data: { ...item.toObject(), commentId }
+    });
+  }
+}
